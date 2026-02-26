a21c3018d35b5e61db9c3a05b7c031ed4f5f2662
PipoGonzalez59 <alexjosemilio@gmail.com>
2026-02-25 20:26:05 -0600

    Ahora retorna string

--- Lector.java
+++ Lector.java
@@ -3,20 +3,33 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class Lector {
+
     String nombreArchivo;
     FileReader archivo;
     BufferedReader lector;
 
-    public void leerArchivo(String nombreArchivo){
+    // ahora retorna un String
+    public String leerArchivo(String nombreArchivo){
+
+        String contenido = "";
+
         try{
-            archivo = new FileReader(nombreArchivo); 
+            archivo = new FileReader(nombreArchivo);
             lector = new BufferedReader(archivo);
+
             String cadena;
+
+            // solo guardamos la primera linea
             while((cadena = lector.readLine()) != null){
-                System.out.println(cadena);
-                }
+                contenido = cadena;
+            }
+
+            lector.close();
+
         } catch(Exception e){
             System.out.println("Error al leer el archivo: " + e.getMessage());
         }
+
+        return contenido;
     }
 }
